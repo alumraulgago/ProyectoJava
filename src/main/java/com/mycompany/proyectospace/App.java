60f92a120a85bc4449f95bc70645ea982b6eb6d9
1DAW01 <1DAW01@192.168.9.112>
2021-03-23 13:28:32 +0100

    RGG - Subida del proyecto space

--- src/main/java/com/mycompany/proyectospace/App.java
+++ src/main/java/com/mycompany/proyectospace/App.java
@@ -1,5 +1,6 @@
 package com.mycompany.proyectospace;
 
+import javafx.animation.KeyFrame;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -10,29 +11,76 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
+import java.util.Random;
 
 
 
 public class App extends Application {
-
+        
+        final short SCENE_TAM_X = 960;
+        short scene_tam_y = 600;
+        byte spacespeedY = 0;
+        byte spacespeedX = 0;
+        short spaceship_posx = 470;
+        short spaceship_posy = 500;
+        int asteroid_posx = 0;
+        short asteroid_posy = -10;
+        short asteroid_widht = 50;
+        short asteroid_height = 50;
+        short background1_y = 0;
+        short background2_y = -600;
+        short backgroundspeed = 2;
+        short velocidad_disparo = 0;
+        short disparo_posx = 470;
+        short disparo_posy = 500;
+ 
+        
     @Override
     public void start(Stage primaryStage) {
+      
+
+
         // Añadir el grupo al contenedor principal
         Pane root = new Pane();
         
         // Crear y mostrar el escenario
-        Scene scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(root, SCENE_TAM_X, scene_tam_y);
         primaryStage.setScene(scene);
         primaryStage.show();
         
+        // Declarar el objeto random
+        Random random = new Random();
+        
         // Cargar la imagen crear objeto ImageView
-        Image img = new Image(getClass().getResourceAsStream("/images/Background-1.png"));
+        Image img = new Image(getClass().getResourceAsStream("/images/backgroundSpace.jpeg"));
         ImageView imgView = new ImageView(img);
         root.getChildren().add(imgView);
-        imgView.setFitWidth(600);
-        imgView.setFitHeight(400);
+        imgView.setFitWidth(SCENE_TAM_X);
+        imgView.setFitHeight(scene_tam_y);
+        imgView.setY(background1_y);
         
-        // Elementos del personaje
+        Image img2 = new Image(getClass().getResourceAsStream("/images/backgroundSpace.jpeg"));
+        ImageView imgView2 = new ImageView(img2);
+        root.getChildren().add(imgView2);
+        imgView2.setFitWidth(SCENE_TAM_X);
+        imgView2.setFitHeight(scene_tam_y);
+        imgView2.setY(background2_y);
+        
+        Image img3 = new Image(getClass().getResourceAsStream("/images/Asteroid Brown.png"));
+        ImageView imgView3 = new ImageView(img3);
+        root.getChildren().add(imgView3);
+        imgView3.setFitWidth(asteroid_widht);
+        imgView3.setFitHeight(asteroid_height);
+        imgView3.setX(asteroid_posx);
+        imgView3.setY(asteroid_posy);
+         
+        
+        /* --- DIBUJO DE LA NAVE --- */
+        // Elementos de la nave
         Rectangle rectangleCuerpoNave = new Rectangle(0, 0, 30, 60);
         Rectangle rectanglePropulsor = new Rectangle(10, -10, 10, 80);
         Rectangle rectangleMotorIzq = new Rectangle(-10, 30, 10, 20);
@@ -48,6 +96,9 @@ public class App extends Application {
         Rectangle rectangleMotorDerFuegoAmarillo = new Rectangle(50, 70, 20, 10);
         Rectangle rectangleMotorDerFuegoRojo = new Rectangle(60, 80, 10, 10);
         Circle circleVentanaCohete = new Circle(15, 13, 9);
+        Circle circleDisparo = new Circle (-15, 25, 5);
+        Circle circleDisparo2 = new Circle (45, 25, 5);
+        
         
         
         // Colorear cada elemento
@@ -66,6 +117,8 @@ public class App extends Application {
         rectangleMotorDerFuegoAmarillo.setFill(Color.YELLOW);
         rectangleMotorDerFuegoRojo.setFill(Color.RED);
         circleVentanaCohete.setFill(Color.AQUA);
+        circleDisparo.setFill(Color.ORANGE);
+        circleDisparo2.setFill(Color.ORANGE);
         
         // Agrupar todos los elementos
         Group groupSpaceShip = new Group();
@@ -85,16 +138,98 @@ public class App extends Application {
         groupSpaceShip.getChildren().add(rectangleMotorDerFuegoRojo);
         groupSpaceShip.getChildren().add(circleVentanaCohete);
         root.getChildren().add(groupSpaceShip);
+        Group groupDisparo = new Group();
+        groupDisparo.getChildren().add(circleDisparo);
+        groupDisparo.getChildren().add(circleDisparo2);
+        root.getChildren().add(groupDisparo);
         
-        // Posicionar el grupo en la pantalla
-        groupSpaceShip.setLayoutX(270);
-        groupSpaceShip.setLayoutY(300);
+        
+        
            
-    }
     
-
+            scene.setOnKeyPressed((KeyEvent event) -> {
+                switch(event.getCode()) {
+                    case UP:
+                        spacespeedY = -6;
+                        break;
+                    case DOWN:
+                        spacespeedY = 6;
+                        break;
+                    case LEFT:
+                        spacespeedX = -6;
+                        break;
+                    case RIGHT:
+                        spacespeedX = 6;
+                        break;
+                    case SPACE:
+                        velocidad_disparo = 6;
+                        break;
+                        
+                }
+            });
+            scene.setOnKeyReleased((KeyEvent event) -> {
+                spacespeedX = 0;
+                spacespeedY = 0;
+                velocidad_disparo = 0;
+            });
+            Timeline timeline;
+            timeline = new Timeline (
+                    new KeyFrame(Duration.seconds(0.017), (ActionEvent ae) -> {
+                        imgView3.setY(asteroid_posy);
+                        imgView3.setX(asteroid_posx);
+                        asteroid_posy+=3;
+                        //System.out.println(asteroid_posy);
+                        if(asteroid_posy >= 600) {
+                            asteroid_posy = -10;
+                            asteroid_posx = random.nextInt(SCENE_TAM_X);
+                            System.out.println("Posición del asteroide:" + asteroid_posx);
+                        }
+                        
+                        spaceship_posy += spacespeedY;
+                        if(spaceship_posy < 10){
+                            spaceship_posy = 10;
+                        } else {
+                            if(spaceship_posy > 505) {
+                                spaceship_posy = 505;
+                            }
+                        }
+                        if(spaceship_posx < 45) {
+                            spaceship_posx = 45;
+                        } else {
+                            if(spaceship_posx > 885) {
+                                spaceship_posx = 885;
+                            }
+                        }
+                        
+                        background1_y += backgroundspeed;
+                        imgView.setY(background1_y);
+                        background2_y += backgroundspeed;
+                        imgView2.setY(background2_y);
+                        
+                        if (background1_y == 600) {
+                            background1_y = -600;
+                        }
+                        if (background2_y == 600) {
+                            background2_y = -600;
+                        }
+                        
+                        
+                        spaceship_posx += spacespeedX;
+                        groupSpaceShip.setLayoutX(spaceship_posx);
+                        groupSpaceShip.setLayoutY(spaceship_posy);
+                        disparo_posy -= velocidad_disparo;
+                        groupDisparo.setLayoutX(disparo_posx);
+                        groupDisparo.setLayoutY(disparo_posy);
+                        if(disparo_posy <= -100) {
+                            disparo_posy = 500;
+                        }
+                        
+                    })
+            );
+            timeline.setCycleCount(Timeline.INDEFINITE);
+            timeline.play();
+    }
     public static void main(String[] args) {
         launch();
     }
-
 }
